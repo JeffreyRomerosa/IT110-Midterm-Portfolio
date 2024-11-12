8b1a6bd5e9a652f7cedf12e608940a27680ca5b6
JeffreyRomerosa <jeffreyromerosa@gmail.com>
2024-11-12 17:02:32 +0800

    WRAPPER3

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -202,7 +202,7 @@ export default {
 <style scoped>
 #wrapper3 {
     background: #0a192f;
-    min-height: 200vh;
+    min-height: 100vh;
     height: max-content;
     display: flex;
     flex-direction: column;
